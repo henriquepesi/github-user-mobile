a105b3abd3eaf0a44e71c7a69f2be54fb22235fd
henrique <henrique.pedasi@gmail.com>
2019-07-29 01:44:40 -0300

    read me update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # github-user-mobile
+
+Project has built with React Native using Github api for looking for github users
+
+## Installation
+
+**Step 1:** clone my repo & cd into project
+
+**Step 2:** install node modules
+
+```
+npm install
+```
+
+*Before run android build, setup [Android Studio](https://facebook.github.io/react-native/docs/android-setup.html)*
+
+**Step 3:** If Android
+
+```
+react-native run-android
+```
+
+*Before running iOS build, Install [Xcode](https://developer.apple.com/xcode/download/)*
+
+**Step 4:** If iOS
+
+```
+react-native run-ios
+```
---
+++ src/pages/Main/index.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+
+export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    newUser: '',
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    console.tron.log(this.props);
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
+  handleAdduser = async () => {
+    const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    // Faz o teclado sumir após a chamada
+    Keyboard.dismiss();
+  };
+
+  // Passar usuário como parametro
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    // Passar o nome da tela que quer enviar o usuário
+    // No seguno parametro passar o usuário referenciado em cima como objeto
+    navigation.navigate('User', { user });
+  };
+
+  render() {
+    const { users, newUser, loading } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome do usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAdduser}
+          />
+          <SubmitButton loading={loading} onPress={this.handleAdduser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List
+          // Dados da lista
+          data={users}
+          // Cria a key no RN
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              {/* Item é o usuário passado como parametro na função */}
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
+      </Container>
+    );
+  }
+}
